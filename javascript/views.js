0bd93ec1f5cd812a33aa9a05a50b8df19dcb2fe2
jeff-frederic <jefferyfrederic@gmail.com>
2023-04-12 22:21:15 -0700

    restarting

--- javascript/views.js
+++
@@ -1,9 +0,0 @@
-
-const canvas = document.getElementById('mazeCanvas');
-const ctx = canvas.getContext('2d');
-
-const rows = 50, columns = 50;
-const cellWidth = Math.ceil(canvas.width/columns);
-const cellHeight = Math.ceil(canvas.height/rows);
-
-export {canvas, ctx, rows, columns, cellHeight, cellWidth};
